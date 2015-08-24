275cd91aea012583eea046ae754e01382f5c06ab
Herb Miller <herb@bobbingwide.com>
2015-08-24 14:19:11 +0100

    v1.14.0 now dependent upon oik v2.4 or higher
    - Responds to oik_add_shortcodes to define the nivo shortcode
    - Responds to admin_notices later than oik might
    - Now depends on oik v2.4 or higher - for symlinked installations

--- oik-nivo-slider.php
+++ oik-nivo-slider.php
@@ -4,12 +4,12 @@ Plugin Name: oik-nivo-slider
 Depends: oik base plugin
 Plugin URI: http://www.oik-plugins.com/oik-plugins/oik-nivo-slider/
 Description: [nivo] shortcode for the Nivo slider using oik
-Version: 1.14
+Version: 1.14.0
 Author: bobbingwide
 Author URI: http://www.bobbingwide.com
 License: GPL2
 
-    Copyright 2012-2014 Bobbing Wide (email : herb@bobbingwide.com )
+    Copyright 2012-2015 Bobbing Wide (email : herb@bobbingwide.com )
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2,
@@ -28,11 +28,10 @@ License: GPL2
 */
 
 /**
- * Implement "oik_loaded" action for the oik-nivo-slider 
+ * Implement "oik_add_shortcodes" action for the oik-nivo-slider 
  */
-function bw_nivo_init() {
+function oik_nivo_add_shortcodes() {
   bw_add_shortcode( "nivo", "bw_nivo_slider", oik_path( "nivo.inc", "oik-nivo-slider" ), false ); 
-  add_action( "admin_menu", "oik_nivo_admin_menu" );
 }
 
 /**
@@ -49,7 +48,7 @@ function oik_nivo_admin_menu() {
  * This code will produce a message when oik-nivo-slider is activated but oik isn't
  *
  * Note: oik-nivo-slider now reports that it's dependent upon oik v2.1
- *   
+ * Note: oik-nivo-slider now dependent upon oik v2.4  
  */ 
 function oik_nivo_activation() {
   static $plugin_basename = null;
@@ -57,9 +56,11 @@ function oik_nivo_activation() {
     $plugin_basename = plugin_basename(__FILE__);
     // bw_trace2( $plugin_basename, "plugin_basename" );
     add_action( "after_plugin_row_oik-nivo-slider/oik-nivo-slider.php" , "oik_nivo_activation" );   
-    require_once( "admin/oik-activation.php" );
+    if ( !function_exists( "oik_plugin_lazy_activation" ) ) { 
+      require_once( "admin/oik-activation.php" );
+    }
   }  
-  $depends = "oik:2.1";
+  $depends = "oik:2.4";
   oik_plugin_lazy_activation( __FILE__, $depends, "oik_plugin_plugin_inactive" );
 }
 
@@ -67,8 +68,9 @@ function oik_nivo_activation() {
  * function to invoke when oik-nivo-slider loaded 
  */                  
 function oik_nivo_loaded() {
-  add_action( "oik_loaded", "bw_nivo_init" );
-  add_action( "admin_notices", "oik_nivo_activation" );
+  add_action( "admin_notices", "oik_nivo_activation", 12 );
+  add_action( "oik_add_shortcodes", "oik_nivo_add_shortcodes" );
+  add_action( "oik_admin_menu", "oik_nivo_admin_menu" );
 }
 
 oik_nivo_loaded();
